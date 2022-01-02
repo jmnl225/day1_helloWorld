ff2ff042efa54c0a30d84c3430b457b9215cd878
joomin <joominl15@gmail.com>
2022-01-02 22:23:08 +0900

    Initial Commit

---
+++ day1_helloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day1_helloWorld
+//
+//  Created by joomin Lee on 2/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
